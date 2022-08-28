2903227d60cc918f0f023d16b69bd0a887a1b4d6
Rémy Roy <github@remyroy.com>
2022-08-28 10:34:47 -0400

    Initial Windows maintenance tasks structure

--- ethwizard/platforms/windows/maintain.py
+++ ethwizard/platforms/windows/maintain.py
@@ -300,8 +300,8 @@ Versions legend - I: Installed, R: Running, L: Latest
         return False
     
     if result == 1:
-        if perform_maintenance(current_execution_client, execution_client_details,
-            current_consensus_client, consensus_client_details):
+        if perform_maintenance(current_directory, current_execution_client,
+            execution_client_details, current_consensus_client, consensus_client_details):
             return show_dashboard(context)
         else:
             log.error('We could not perform all the maintenance tasks.')
@@ -607,10 +607,10 @@ def get_teku_latest_version():
 
     log.info('Getting Teku latest version...')
 
-    lighthouse_gh_release_url = GITHUB_REST_API_URL + TEKU_LATEST_RELEASE
+    teku_gh_release_url = GITHUB_REST_API_URL + TEKU_LATEST_RELEASE
     headers = {'Accept': GITHUB_API_VERSION}
     try:
-        response = httpx.get(lighthouse_gh_release_url, headers=headers,
+        response = httpx.get(teku_gh_release_url, headers=headers,
             follow_redirects=True)
     except httpx.RequestError as exception:
         log.error(f'Exception while getting the latest stable version for Teku. {exception}')
@@ -662,8 +662,454 @@ def use_default_client(context):
 
     return context
 
-def perform_maintenance(execution_client, execution_client_details, consensus_client,
-    consensus_client_details):
-    # TODO: Perform all the maintenance tasks
+def perform_maintenance(base_directory, execution_client, execution_client_details,
+    consensus_client, consensus_client_details):
+    # Perform all the maintenance tasks
 
+    base_directory = Path(base_directory)
+
+    nssm_binary = get_nssm_binary()
+    if not nssm_binary:
+        return False
+
+    if execution_client == EXECUTION_CLIENT_GETH:
+        # Geth maintenance tasks
+        geth_service_name = 'geth'
+
+        if execution_client_details['next_step'] == MAINTENANCE_RESTART_SERVICE:
+            log.info('Restarting Geth service...')
+
+            subprocess.run([str(nssm_binary), 'restart', geth_service_name])
+
+        elif execution_client_details['next_step'] == MAINTENANCE_UPGRADE_CLIENT:
+            if not upgrade_geth(base_directory, nssm_binary):
+                log.error('We could not upgrade the Geth client.')
+                return False
+        
+        elif execution_client_details['next_step'] == MAINTENANCE_UPGRADE_CLIENT_MERGE:
+            if not config_geth_merge(base_directory, nssm_binary):
+                log.error('We could not configure Geth for the merge.')
+                return False
+            
+            if not upgrade_geth(base_directory, nssm_binary):
+                log.error('We could not upgrade the Geth client.')
+                return False
+    
+        elif execution_client_details['next_step'] == MAINTENANCE_CONFIG_CLIENT_MERGE:
+            if not config_geth_merge(base_directory, nssm_binary):
+                log.error('We could not configure Geth for the merge.')
+                return False
+            
+            log.info('Restarting Geth service...')
+
+            subprocess.run([str(nssm_binary), 'restart', geth_service_name])
+
+        elif execution_client_details['next_step'] == MAINTENANCE_START_SERVICE:
+            log.info('Starting Geth service...')
+
+            subprocess.run([str(nssm_binary), 'start', geth_service_name])
+
+        elif execution_client_details['next_step'] == MAINTENANCE_REINSTALL_CLIENT:
+            log.warn('TODO: Reinstalling client is to be implemented.')
+    else:
+        log.error(f'Unknown execution client {execution_client}.')
+        return False
+    
+    if consensus_client == CONSENSUS_CLIENT_TEKU:
+        # Teku maintenance tasks
+        teku_service_name = 'teku'
+
+        if consensus_client_details['next_step'] == MAINTENANCE_RESTART_SERVICE:
+            log.info('Restarting Teku service...')
+
+            subprocess.run([str(nssm_binary), 'restart', teku_service_name])
+
+        elif consensus_client_details['next_step'] == MAINTENANCE_UPGRADE_CLIENT:
+            if not upgrade_teku(base_directory, nssm_binary):
+                log.error('We could not upgrade the Teku client.')
+                return False
+        
+        elif consensus_client_details['next_step'] == MAINTENANCE_UPGRADE_CLIENT_MERGE:
+            if not config_teku_merge(base_directory, nssm_binary):
+                log.error('We could not configure Teku for the merge.')
+                return False
+
+            if not upgrade_teku(base_directory, nssm_binary):
+                log.error('We could not upgrade the Teku client.')
+                return False
+    
+        elif consensus_client_details['next_step'] == MAINTENANCE_CONFIG_CLIENT_MERGE:
+            if not config_teku_merge(base_directory, nssm_binary):
+                log.error('We could not configure Teku for the merge.')
+                return False
+            
+            log.info('Restarting Teku service...')
+
+            subprocess.run([str(nssm_binary), 'restart', teku_service_name])
+            
+        elif consensus_client_details['next_step'] == MAINTENANCE_START_SERVICE:
+            log.info('Starting Teku service...')
+
+            subprocess.run([str(nssm_binary), 'start', teku_service_name])
+
+        elif consensus_client_details['next_step'] == MAINTENANCE_REINSTALL_CLIENT:
+            log.warn('TODO: Reinstalling client is to be implemented.')
+    else:
+        log.error(f'Unknown consensus client {consensus_client}.')
+        return False
+
+    return True
+
+def upgrade_geth(base_directory, nssm_binary):
+    # Upgrade the Geth client
+    log.info('Upgrading Geth client...')
+    # TODO: Implement
+
+    """# Add Ethereum PPA if not already added.
+    if not is_ethereum_ppa_added():
+        spc_package_installed = False
+        try:
+            spc_package_installed = is_package_installed('software-properties-common')
+        except Exception:
+            return False
+        
+        if not spc_package_installed:
+            subprocess.run([
+                'apt', '-y', 'update'])
+            subprocess.run([
+                'apt', '-y', 'install', 'software-properties-common'])
+
+        subprocess.run(['add-apt-repository', '-y', 'ppa:ethereum/ethereum'])
+    else:
+        subprocess.run(['apt', '-y', 'update'])
+
+    env = os.environ.copy()
+    env['DEBIAN_FRONTEND'] = 'noninteractive'
+
+    subprocess.run(['apt', '-y', 'install', 'geth'], env=env)
+
+    log.info('Restarting Geth service...')
+    subprocess.run(['systemctl', 'restart', GETH_SYSTEMD_SERVICE_NAME])
+
+    return True"""
+    return False
+
+def config_geth_merge(base_directory, nssm_binary):
+    # Configure Geth for the merge
+    log.info('Configuring Geth for the merge...')
+    # TODO: Implemention
+
+    """log.info('Creating JWT token file if needed...')
+    if not setup_jwt_token_file():
+        log.error(
+f'''
+Unable to create JWT token file in {LINUX_JWT_TOKEN_FILE_PATH}
+'''
+        )
+
+        return False
+    
+    geth_service_name = GETH_SYSTEMD_SERVICE_NAME
+    geth_service_content = ''
+
+    log.info('Adding JWT token configuration to Geth...')
+
+    with open('/etc/systemd/system/' + geth_service_name, 'r') as service_file:
+        geth_service_content = service_file.read()
+
+    result = re.search(r'ExecStart\s*=\s*(.*?)geth([^\\\n]*(\\\s+)?)*', geth_service_content)
+    if not result:
+        log.error('Cannot parse Geth service file.')
+        return False
+    
+    exec_start = result.group(0)
+
+    # Add --authrpc.jwtsecret configuration
+    exec_start = re.sub(r'(\s*\\)?\s+--authrpc.jwtsecret\s*=?\s*\S+', '', exec_start)
+    exec_start = exec_start + f' --authrpc.jwtsecret {LINUX_JWT_TOKEN_FILE_PATH}'
+
+    geth_service_content = re.sub(r'ExecStart\s*=\s*(.*?)geth([^\\\n]*(\\\s+)?)*',
+        exec_start, geth_service_content)
+
+    # Write back configuration
+    with open('/etc/systemd/system/' + geth_service_name, 'w') as service_file:
+        service_file.write(geth_service_content)
+
+    # Reload configuration
+    log.info('Reloading service configurations...')
+    subprocess.run(['systemctl', 'daemon-reload'])
+
+    return True"""
+    return False
+
+def upgrade_teku(base_directory, nssm_binary):
+    # Upgrade the Teku client
+    log.info('Upgrading Teku client...')
+    # TODO: Implemention
+
+    """# Getting latest Teku release files
+    teku_gh_release_url = GITHUB_REST_API_URL + TEKU_LATEST_RELEASE
+    headers = {'Accept': GITHUB_API_VERSION}
+    try:
+        response = httpx.get(teku_gh_release_url, headers=headers,
+            follow_redirects=True)
+    except httpx.RequestError as exception:
+        log.error(f'Exception while downloading teku binary. {exception}')
+        return False
+
+    if response.status_code != 200:
+        log.error(f'HTTP error while downloading teku binary. '
+            f'Status code {response.status_code}')
+        return False
+    
+    release_json = response.json()
+
+    if 'assets' not in release_json:
+        log.error('No assets in Github release for teku.')
+        return False
+    
+    binary_asset = None
+    signature_asset = None
+
+    archive_filename_comp = 'x86_64-unknown-linux-gnu.tar.gz'
+
+    use_optimized_binary = is_adx_supported()
+    if not use_optimized_binary:
+        log.warn('CPU does not support ADX instructions. '
+            'Using the portable version for Teku.')
+        archive_filename_comp = 'x86_64-unknown-linux-gnu-portable.tar.gz'
+    
+    archive_filename_sig_comp = archive_filename_comp + '.asc'
+
+    for asset in release_json['assets']:
+        if 'name' not in asset:
+            continue
+        if 'browser_download_url' not in asset:
+            continue
+
+        file_name = asset['name']
+        file_url = asset['browser_download_url']
+
+        if file_name.endswith(archive_filename_comp):
+            binary_asset = {
+                'file_name': file_name,
+                'file_url': file_url
+            }
+        elif file_name.endswith(archive_filename_sig_comp):
+            signature_asset = {
+                'file_name': file_name,
+                'file_url': file_url
+            }
+
+    if binary_asset is None or signature_asset is None:
+        log.error('Could not find binary or signature asset in Github release.')
+        return False
+    
+    # Downloading latest Teku release files
+    download_path = Path(Path.home(), 'ethwizard', 'downloads')
+    download_path.mkdir(parents=True, exist_ok=True)
+
+    binary_path = Path(download_path, binary_asset['file_name'])
+
+    try:
+        with open(binary_path, 'wb') as binary_file:
+            with httpx.stream('GET', binary_asset['file_url'],
+                follow_redirects=True) as http_stream:
+                if http_stream.status_code != 200:
+                    log.error(f'HTTP error while downloading Teku binary from Github. '
+                        f'Status code {http_stream.status_code}')
+                    return False
+                for data in http_stream.iter_bytes():
+                    binary_file.write(data)
+    except httpx.RequestError as exception:
+        log.error(f'Exception while downloading Teku binary from Github. {exception}')
+        return False
+    
+    signature_path = Path(download_path, signature_asset['file_name'])
+
+    try:
+        with open(signature_path, 'wb') as signature_file:
+            with httpx.stream('GET', signature_asset['file_url'],
+                follow_redirects=True) as http_stream:
+                if http_stream.status_code != 200:
+                    log.error(f'HTTP error while downloading Teku signature from Github. '
+                        f'Status code {http_stream.status_code}')
+                    return False
+                for data in http_stream.iter_bytes():
+                    signature_file.write(data)
+    except httpx.RequestError as exception:
+        log.error(f'Exception while downloading Teku signature from Github. {exception}')
+        return False
+
+    # Test if gpg is already installed
+    gpg_is_installed = False
+    try:
+        gpg_is_installed = is_package_installed('gpg')
+    except Exception:
+        return False
+
+    if not gpg_is_installed:
+        # Install gpg using APT
+        subprocess.run([
+            'apt', '-y', 'update'])
+        subprocess.run([
+            'apt', '-y', 'install', 'gpg'])
+
+    # Verify PGP signature
+
+    command_line = ['gpg', '--list-keys', '--with-colons', LIGHTHOUSE_PRIME_PGP_KEY_ID]
+    process_result = subprocess.run(command_line)
+    pgp_key_found = process_result.returncode == 0
+
+    if not pgp_key_found:
+        retry_index = 0
+        retry_count = 15
+
+        key_server = PGP_KEY_SERVERS[retry_index % len(PGP_KEY_SERVERS)]
+        log.info(f'Downloading Sigma Prime\'s PGP key from {key_server} ...')
+        command_line = ['gpg', '--keyserver', key_server, '--recv-keys',
+            LIGHTHOUSE_PRIME_PGP_KEY_ID]
+        process_result = subprocess.run(command_line)
+
+        if process_result.returncode != 0:
+            # GPG failed to download Sigma Prime's PGP key, let's wait and retry a few times
+            while process_result.returncode != 0 and retry_index < retry_count:
+                retry_index = retry_index + 1
+                delay = 5
+                log.warning(f'GPG failed to download the PGP key. We will wait {delay} seconds '
+                    f'and try again from a different server.')
+                time.sleep(delay)
+
+                key_server = PGP_KEY_SERVERS[retry_index % len(PGP_KEY_SERVERS)]
+                log.info(f'Downloading Sigma Prime\'s PGP key from {key_server} ...')
+                command_line = ['gpg', '--keyserver', key_server, '--recv-keys',
+                    LIGHTHOUSE_PRIME_PGP_KEY_ID]
+
+                process_result = subprocess.run(command_line)
+        
+        if process_result.returncode != 0:
+            log.error(
+f'''
+We failed to download the Sigma Prime's PGP key to verify the teku
+binary after {retry_count} retries.
+'''
+            )
+            return False
+    
+    process_result = subprocess.run([
+        'gpg', '--verify', signature_path])
+    if process_result.returncode != 0:
+        log.error('The teku binary signature is wrong. '
+            'We will stop here to protect you.')
+        return False
+    
+    # Stopping Teku services before updating the binary
+    log.info('Stopping Teku services...')
+    subprocess.run(['systemctl', 'stop', LIGHTHOUSE_BN_SYSTEMD_SERVICE_NAME,
+        LIGHTHOUSE_VC_SYSTEMD_SERVICE_NAME])
+
+    # Extracting the Teku binary archive
+    log.info('Updating Teku binary...')
+    subprocess.run([
+        'tar', 'xvf', binary_path, '--directory', LIGHTHOUSE_INSTALLED_DIRECTORY])
+    
+    # Restarting Teku services after updating the binary
+    log.info('Starting Teku services...')
+    subprocess.run(['systemctl', 'start', LIGHTHOUSE_BN_SYSTEMD_SERVICE_NAME,
+        LIGHTHOUSE_VC_SYSTEMD_SERVICE_NAME])
+
+    # Remove download leftovers
+    binary_path.unlink()
+    signature_path.unlink()
+
+    return True"""
+    return False
+
+def config_teku_merge(base_directory, nssm_binary):
+    # Configure Teku for the merge
+    log.info('Configuring Teku for the merge...')
+    # TODO: Implemention
+
+    """fee_recipient_address = select_fee_recipient_address()
+    if not fee_recipient_address:
+        log.error('No fee recipient address entered.')
+        return False
+
+    log.info('Creating JWT token file if needed...')
+    if not setup_jwt_token_file():
+        log.error(
+f'''
+Unable to create JWT token file in {LINUX_JWT_TOKEN_FILE_PATH}
+'''
+        )
+
+        return False
+    
+    # Configure the Teku beacon node
+
+    teku_bn_service_name = LIGHTHOUSE_BN_SYSTEMD_SERVICE_NAME
+    teku_bn_service_content = ''
+
+    log.info('Adding JWT token configuration to Teku beacon node and '
+        'using the correct API port...')
+
+    with open('/etc/systemd/system/' + teku_bn_service_name, 'r') as service_file:
+        teku_bn_service_content = service_file.read()
+
+    result = re.search(r'ExecStart\s*=\s*(.*?)teku([^\\\n]*(\\\s+)?)*', teku_bn_service_content)
+    if not result:
+        log.error('Cannot parse Teku beacon node service file.')
+        return False
+    
+    exec_start = result.group(0)
+
+    # Remove all --eth1-endpoints related configuration
+    exec_start = re.sub(r'(\s*\\)?\s+--eth1-endpoints?\s*=?\s*\S+', '', exec_start)
+
+    # Add --execution-endpoint configuration
+    exec_start = re.sub(r'(\s*\\)?\s+--execution-endpoints?\s*=?\s*\S+', '', exec_start)
+    exec_start = exec_start + ' --execution-endpoint http://127.0.0.1:8551'
+
+    # Add --execution-jwt configuration
+    exec_start = re.sub(r'(\s*\\)?\s+--execution-jwt\s*=?\s*\S+', '', exec_start)
+    exec_start = exec_start + f' --execution-jwt {LINUX_JWT_TOKEN_FILE_PATH}'
+
+    teku_bn_service_content = re.sub(r'ExecStart\s*=\s*(.*?)teku([^\\\n]*(\\\s+)?)*',
+        exec_start, teku_bn_service_content)
+
+    # Write back configuration
+    with open('/etc/systemd/system/' + teku_bn_service_name, 'w') as service_file:
+        service_file.write(teku_bn_service_content)
+
+    # Configure the Teku validator client
+
+    teku_vc_service_name = LIGHTHOUSE_VC_SYSTEMD_SERVICE_NAME
+    teku_vc_service_content = ''
+
+    with open('/etc/systemd/system/' + teku_vc_service_name, 'r') as service_file:
+        teku_vc_service_content = service_file.read()
+    
+    result = re.search(r'ExecStart\s*=\s*(.*?)teku([^\\\n]*(\\\s+)?)*', teku_vc_service_content)
+    if not result:
+        log.error('Cannot parse Teku validator client service file.')
+        return False
+
+    exec_start = result.group(0)
+
+    # Add fee recipient address
+    exec_start = re.sub(r'(\s*\\)?\s+--suggested-fee-recipient\s*=?\s*\S+', '', exec_start)
+    exec_start = exec_start + f' --suggested-fee-recipient {fee_recipient_address}'
+    
+    teku_vc_service_content = re.sub(r'ExecStart\s*=\s*(.*?)teku([^\\\n]*(\\\s+)?)*',
+        exec_start, teku_vc_service_content)
+
+    # Write back configuration
+    with open('/etc/systemd/system/' + teku_vc_service_name, 'w') as service_file:
+        service_file.write(teku_vc_service_content)
+
+    # Reload configuration
+    log.info('Reloading service configurations...')
+    subprocess.run(['systemctl', 'daemon-reload'])
+
+    return True"""
     return False
